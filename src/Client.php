928dce1c9c82b47ec72cdabc5f1ec85376b00ff8
Aleksandr Safronov <me@safron.pro>
2019-06-16 18:35:15 +0300

    Client: setters for type, sender and gate added

--- src/Client.php
+++ src/Client.php
@@ -3,16 +3,26 @@ namespace Leadsapi\Gate;
 
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\RequestException as HttpRequestException;
+use GuzzleHttp\Exception\{
+    RequestException as HttpRequestException,
+    GuzzleException
+};
 
 
 class Client {
 
 	const DEF_ENDPOINT = 'https://gate.leadsapi.org';
+	const TYPE_SMS = 'sms';
+	const TYPE_VIBER = 'viber';
+	const TYPE_EMAIL = 'email';
 
 	private $user;
 	private $token;
 	private $endpoint;
+	private $sendUrl = '/send/sms';
+	private $type = self::TYPE_SMS;
+	private $sender = '';
+	private $gate = '';
 
 	private $cli;
 
@@ -21,20 +31,54 @@ class Client {
 		$this->user = $user;
 		$this->token = $token;
 		$this->endpoint = rtrim($endpoint, '/');
+		$this->rebuildSendUrl();
 	}
 
-	/**
-	 * @param string $phone
-	 * @param string $text
-	 * @return Result
-	 * @throws Exception
-	 */
+    /**
+     * @param string $type - One of [TYPE_SMS, TYPE_VIBER, TYPE_EMAIL]
+     * @return Client
+     */
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        $this->rebuildSendUrl();
+        return $this;
+    }
+
+    /**
+     * @param string $sender
+     * @return Client
+     */
+    public function setSender(string $sender): self
+    {
+        $this->sender = $sender;
+        $this->rebuildSendUrl();
+        return $this;
+    }
+
+    /**
+     * @param string $gate
+     * @return Client
+     */
+    public function setGate(string $gate): self
+    {
+        $this->gate = $gate;
+        $this->rebuildSendUrl();
+        return $this;
+    }
+
+    /**
+     * @param string $phone
+     * @param string $text
+     * @return Result
+     * @throws Exception
+     */
 	public function sendSms(string $phone, string $text): Result
 	{
 	    $result = $this->send(
 	        new Request(
 	            'POST',
-	            '/send/sms',
+	            $this->sendUrl,
 	            ['Content-Type' => 'application/json'],
 	            json_encode(['target' => $phone, 'text' => $text])
 	        )
@@ -45,11 +89,11 @@ class Client {
 	    return new Result($result['sending_id']);
 	}
 
-	/**
-	 * @param iterable $messages
-	 * @return BulkResult
-	 * @throws Exception
-	 */
+    /**
+     * @param iterable $messages
+     * @return BulkResult
+     * @throws Exception
+     */
 	public function sendSmsBulk(iterable $messages): BulkResult {
 	    $body = fopen('php://temp', 'r+');
 	    foreach ($messages as [$phone, $text]) {
@@ -57,22 +101,22 @@ class Client {
 	    }
 	    rewind($body);
 	    try {
-	        $resp = $this->send(
-	            new Request('POST', '/send/sms', ['Content-Type' => 'text/tab-separated-values'], $body)
-	        );
-	        if (!isset($resp['bulk_id'])) {
-	    		throw new Exception("No id in result");
-	    	}
-	    	$result = new BulkResult($resp['bulk_id']);
-	    	$result->enqueued = $resp['enqueued'] ?? 0;
-	    	$result->errors = $resp['errors'] ?? [];
-	    	return $result;
+            $resp = $this->send(
+                new Request('POST', $this->sendUrl, ['Content-Type' => 'text/tab-separated-values'], $body)
+            );
+            if (!isset($resp['bulk_id'])) {
+                throw new Exception("No id in result");
+            }
+            $result = new BulkResult($resp['bulk_id']);
+            $result->enqueued = $resp['enqueued'] ?? 0;
+            $result->errors = $resp['errors'] ?? [];
+            return $result;
 	    } finally {
 	        @fclose($body);
 	    }
 	}
 
-	protected function getHttpClient(): HttpClient
+	private function getHttpClient(): HttpClient
 	{
 		if (!$this->cli) {
 			$this->cli = new HttpClient([
@@ -83,7 +127,12 @@ class Client {
 	    return $this->cli;
 	}
 
-	protected function send(Request $request): array {
+    /**
+     * @param Request $request
+     * @return array
+     * @throws Exception
+     */
+	private function send(Request $request): array {
 	    try {
 	        $response = $this->getHttpClient()->send($request);
 	        $data = json_decode($response->getBody()->getContents(), true);
@@ -96,7 +145,19 @@ class Client {
 	            "Error sending: %s\n",
 	            ($response = $e->getResponse()) ? $response->getBody()->getContents() : $e->getMessage()
 	        ));
-	    }
+	    } catch (GuzzleException $e) {
+	        throw new Exception(sprintf("Error sending: %s\n", $e->getMessage()));
+        }
 	}
+
+	private function rebuildSendUrl() {
+	    $this->sendUrl = sprintf(
+	        '/send/%s%s%s',
+            $this->type,
+            $this->gate ? "/{$this->gate}" : '',
+            $this->sender ? "?sender={$this->sender}" : ''
+        );
+    }
+
 
 }
